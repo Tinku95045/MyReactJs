3bef082e8e8115758e5d22a559fc2350e4447c06
Tinku Kumar <tinkukumar@Tinkus-MacBook-Air.local>
2021-03-07 17:15:06 +0530

    HelloReact.js file is added

---
+++ HelloReact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+function Hello()
+{
+    render()
+    {
+        return(<h3>Hello React!!!</h3>);
+    }
+}
+
+export default Hello;
